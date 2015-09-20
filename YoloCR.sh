77174361023e6568d7f7f4cb0f89a878478bdc5f
Yuri Zero <yuri.zero@gmx.fr>
2015-09-20 15:32:25 +0200

    Fix bug timing and cosmetics.

--- YoloCR.sh
+++ YoloCR.sh
@@ -26,8 +26,8 @@ if [ -d TessResult ]; then rm TessResult/*.hocr TessResult/*.txt 2> /dev/null; e
 if [ -f SceneChangesAlt.log ]; then bAlt=true; else bAlt=false; rm TimecodesAlt.txt 2> /dev/null; fi
 if [[ $(head -3 SceneChanges.log | tail -1 | cut -d' ' -f2-) != $(head -4 SceneChanges.log | tail -1 | cut -d' ' -f2-) ]]; then tailnum=3; else tailnum=4; fi # Workaround bug SceneChangeDetection
 FPS=$(ffprobe "$FilteredVideo" -v 0 -select_streams v -print_format flat -show_entries stream=r_frame_rate | cut -d'"' -f2 | bc -l)
-tail -n +$tailnum SceneChanges.log | awk -v fps=$FPS '{if ($3 == 0) printf "%.3f\n", $1/fps; else if ($2 == 0) {var=sprintf ("%.4f", ($1+1)/fps); print substr(var, 0, length(var)-1)}}' | sort -n > Timecodes.txt &
-    if $bAlt; then tail -n +$tailnum SceneChangesAlt.log | awk -v fps=$FPS '{if ($3 == 0) printf "%.3f\n", $1/fps; else if ($2 == 0) {var=sprintf ("%.4f", ($1+1)/fps); print substr(var, 0, length(var)-1)}}' | sort -n > TimecodesAlt.txt; fi
+tail -n +$tailnum SceneChanges.log | awk -v fps=$FPS '{if ($3 == 0) {var=sprintf ("%.4f", $1/fps); print substr(var, 0, length(var)-1)} else if ($2 == 0) {var=sprintf ("%.4f", ($1+1)/fps); print substr(var, 0, length(var)-1)}}' | sort -n > Timecodes.txt &
+    if $bAlt; then tail -n +$tailnum SceneChangesAlt.log | awk -v fps=$FPS '{if ($3 == 0) {var=sprintf ("%.4f", $1/fps); print substr(var, 0, length(var)-1)} else if ($2 == 0) {var=sprintf ("%.4f", ($1+1)/fps); print substr(var, 0, length(var)-1)}}' | sort -n > TimecodesAlt.txt; fi
         wait
 if parallel --minversion 20131122 1> /dev/null; then popt='--bar'; else popt='--eta'; fi
 TessVersionNum=$(tesseract -v 2>&1 | head -1 | cut -d' ' -f2)
@@ -153,9 +153,7 @@ for SRT in $(printf OCR%s.srt\\n "" $Alt); do {
     if [[ $lang = fra ]]; then
         sed "s/I'/l'/g" $SRT |
         sed 's/[®©]/O/g' |
-        sed 's/\([cs]\)oeur/\1œur/g' |
-        sed 's/oeuvre/œuvre/g' |
-        sed 's/oeuf/œuf/g' |
+        sed -e 's/\([cs]\)oeur/\1œur/g' -e 's/oeuvre/œuvre/g' -e 's/oeuf/œuf/g' |
         sed "s/ *'/’/g" |
         sed 's/\(.\)—\(.\)/\1-\2/g' |
         sed 's/- /— /g' |
