4a7a7df38f9a2c24c3d05f1681f8cccb0391d72c
Yuri Zero <yuri.zer0@yandex.com>
2018-03-23 00:58:48 +0100

    Update the installation script for Debian Stretch. Drop Jessie and Betsy support. Multithreaded building.

--- YoloDebInstallation.sh
+++ YoloDebInstallation.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 ##
-## Script d'installation des prérequis du script YoloCR, pour Debian Jessie et LMDE Betsy
+## Script d'installation des prérequis du script YoloCR, pour Debian Stretch.
 ## Avec l'aimable participation de Cédric Villani et Nightwish : 
 ## http://www.huffingtonpost.fr/2015/05/07/cedric-villani-2025-plus-de-problemes-courage-fierte_n_7220124.html
 
 release=$(lsb_release -a 2>&1 | grep Codename | cut -f2)
-if [[ $release != jessie && $release != betsy ]]
+if [[ $release != stretch ]]
 	then if [[ $1 != eng-only ]]
-		then echo "Ce script ne fonctionne que sur Debian Jessie et LMDE Betsy."
-		else echo "This script only works on Debian Jessie and LMDE Betsy."
+		then echo "Ce script ne fonctionne que sur Debian Stretch."
+		else echo "This script only works on Debian Stretch."
 	fi; exit
 fi
 
@@ -16,33 +16,25 @@ if [[ $1 != eng-only ]]; then tesseractfra=tesseract-ocr-fra; fi
 Desktop=$(grep DESKTOP /home/$USER/.config/user-dirs.dirs 2>/dev/null | cut -d/ -f2 | rev | cut -c 2- | rev)
 if [ -z $Desktop ]; then Desktop=Desktop; fi
 
-if [[ ! $(apt-cache policy | grep "Debian Backports") ]]; then
-    if [[ $release = jessie ]]; then su -c "echo -e '\n# Debian Backports\ndeb http://ftp.debian.org/debian jessie-backports main' >> /etc/apt/sources.list"
-    else su -c "echo -e 'deb http://ftp.debian.org/debian jessie-backports main' >> /etc/apt/sources.list.d/additional-repositories.list"; fi
-fi 
-if [[ $release = jessie && ! $(apt-cache policy | grep "Unofficial Multimedia Packages") ]]; then
-	su -c "echo -e '\n# Debian Multimedia\ndeb http://www.deb-multimedia.org jessie main non-free' >> /etc/apt/sources.list
-	apt update; apt install deb-multimedia-keyring
-	apt update; apt dist-upgrade"
-fi
-su -c "apt install curl tesseract-ocr $tesseractfra links sxiv xdotool parallel ffmpeg git build-essential autoconf automake libtool pkg-config nasm python3-dev cython3 libffms2-3 bsdtar qtbase5-dev qt5-qmake"
-su -c "apt -t jessie-backports install cython3"
+su -c "apt install curl tesseract-ocr $tesseractfra links sxiv xdotool parallel ffmpeg git build-essential autoconf automake libtool pkg-config yasm python3-dev cython3 libffms2-4 bsdtar qtbase5-dev qt5-qmake libqt5websockets5-dev"
+nasmurl=http://ftp.debian.org/debian/pool/main/n/nasm/
+nasmdeb=$(links -dump $nasmurl | grep _amd64 | tail -1 | awk '{ print $3; }')
+wget $nasmurl$nasmdeb; su -c "dpkg -i $nasmdeb"; rm $nasmdeb
 mkdir Gits; cd Gits
 
 # Installation de zimg
 git clone https://github.com/sekrit-twc/zimg.git; cd zimg
-./autogen.sh && ./configure && make
+./autogen.sh && ./configure && make -j$(nproc)
 su -c "make install"; cd ..
 
 # Installation de Vapoursynth
 git clone https://github.com/vapoursynth/vapoursynth.git; cd vapoursynth
-./autogen.sh && ./configure && make
+./autogen.sh && ./configure && make -j$(nproc)
 su -c "make install"; cd ..
 
 # Installation de Vapoursynth Editor
-#git clone https://bitbucket.org/mystery_keeper/vapoursynth-editor.git; cd vapoursynth-editor/pro
-wget https://bitbucket.org/mystery_keeper/vapoursynth-editor/get/r16.tar.bz2; bsdtar -xf r16.tar.bz2; rm r16.tar.bz2; cd mystery_keeper-vapoursynth-editor-3a304dd94528/pro
-qmake -qt5 && make; cd ..
+git clone https://bitbucket.org/mystery_keeper/vapoursynth-editor.git; cd vapoursynth-editor/pro
+qmake -qt5 && make -j$(nproc); cd ..
 su -c "cp build/release-64bit-gcc/vsedit /usr/local/bin/vsedit 
 install -D build/release-64bit-gcc/vsedit.svg /usr/local/share/pixmaps/vsedit.svg
 if [ ! -d /usr/local/share/applications ]; then mkdir /usr/local/share/applications; fi
@@ -51,32 +43,32 @@ cp /usr/local/share/applications/vsedit.desktop /home/$USER/$Desktop/vsedit.desk
 cd ..
 
 # Création du lien symbolique FFMS2 dans le dossier plugins de Vapoursynth
-su -c "ln -s $(dpkg-query -L libffms2-3 | tail -1) /usr/local/lib/vapoursynth/libffms2.so"
+su -c "ln -s $(dpkg-query -L libffms2-4 | grep libffms2.so | tail -1) /usr/local/lib/vapoursynth/libffms2.so"
 
 # Installation de HAvsFunc, mvsfunc et adjust
 git clone https://github.com/HomeOfVapourSynthEvolution/havsfunc.git
 git clone https://github.com/HomeOfVapourSynthEvolution/mvsfunc.git
 git clone https://github.com/dubhater/vapoursynth-adjust.git
-su -c "cp havsfunc/havsfunc.py mvsfunc/mvsfunc.py vapoursynth-adjust/adjust.py /usr/local/lib/python3.4/site-packages/"
+su -c "cp havsfunc/havsfunc.py mvsfunc/mvsfunc.py vapoursynth-adjust/adjust.py /usr/local/lib/python3.5/site-packages/"
 
 # Installation de fmtconv
 git clone https://github.com/EleonoreMizo/fmtconv.git; cd fmtconv/build/unix
-./autogen.sh && ./configure --libdir=/usr/local/lib/vapoursynth && make
+./autogen.sh && ./configure --libdir=/usr/local/lib/vapoursynth && make -j$(nproc)
 su -c "make install"; cd ../../..
 
 # Installation de nnedi3
 git clone https://github.com/dubhater/vapoursynth-nnedi3.git; cd vapoursynth-nnedi3
-./autogen.sh && ./configure --libdir=/usr/local/lib/vapoursynth && make
+./autogen.sh && ./configure --libdir=/usr/local/lib/vapoursynth && make -j$(nproc)
 su -c "make install"; cd ..
 
 # Installation de edi_rpow2
 git clone https://gist.github.com/020c497524e794779d9c.git vapoursynth-edi_rpow2
-su -c "cp vapoursynth-edi_rpow2/edi_rpow2.py /usr/local/lib/python3.4/site-packages/edi_rpow2.py"
+su -c "cp vapoursynth-edi_rpow2/edi_rpow2.py /usr/local/lib/python3.5/site-packages/edi_rpow2.py"
 
 # Vapoursynth doit fonctionner
 if [[ $1 != eng-only ]]
-then echo -e '\n# Nécessaire pour Vapoursynth\nexport PYTHONPATH=/usr/local/lib/python3.4/site-packages' >> ~/.bashrc
-else echo -e '\n# Required for Vapoursynth\nexport PYTHONPATH=/usr/local/lib/python3.4/site-packages' >> ~/.bashrc
+then echo -e '\n# Nécessaire pour Vapoursynth\nexport PYTHONPATH=/usr/local/lib/python3.5/site-packages' >> ~/.bashrc
+else echo -e '\n# Required for Vapoursynth\nexport PYTHONPATH=/usr/local/lib/python3.5/site-packages' >> ~/.bashrc
 fi
 
 # Éviter un reboot
