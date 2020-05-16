b370181c923be5f9905f843c52bb377555909949
Yuri Zero <yuri.zer0@yandex.com>
2020-05-16 19:38:36 +0200

    Installation script now requires Ubuntu 20.04.

--- YoloBuntuInstallation.sh
+++ YoloBuntuInstallation.sh
@@ -5,10 +5,10 @@
 ## http://www.huffingtonpost.fr/2015/05/07/cedric-villani-2025-plus-de-problemes-courage-fierte_n_7220124.html
 
 release=$(lsb_release -a 2>&1 | grep Codename | cut -f2)
-if [[ $release != stretch ]]
+if [[ $release != focal ]]
 	then if [[ $1 != eng-only ]]
-		then echo "Ce script ne fonctionne que sur Debian Stretch."
-		else echo "This script only works on Debian Stretch."
+		then echo "Ce script ne fonctionne que sur Ubuntu 20.04."
+		else echo "This script only works on Ubuntu 20.04."
 	fi; exit
 fi
 
@@ -26,67 +26,65 @@ if [ ! -z $DISPLAY ]; then
 	DesktopPkg="sxiv xdotool qtbase5-dev qt5-qmake libqt5websockets5-dev"
 fi
 
-su -c "apt install bc gawk curl tesseract-ocr $tesseractfra links parallel ffmpeg git build-essential autoconf automake libtool pkg-config yasm python3-dev cython3 libffms2-4 bsdtar $DesktopPkg"
+sudo apt install bc gawk curl tesseract-ocr $tesseractfra links parallel ffmpeg git build-essential autoconf automake libtool pkg-config yasm python3-dev cython3 libffms2-4 libarchive-tools $DesktopPkg
 nasmurl=http://ftp.debian.org/debian/pool/main/n/nasm/
 nasmdeb=$(links -dump $nasmurl | grep _amd64 | tail -1 | awk '{ print $3; }')
-wget $nasmurl$nasmdeb; su -c "dpkg -i $nasmdeb"; rm $nasmdeb
+wget $nasmurl$nasmdeb; sudo dpkg -i $nasmdeb; rm $nasmdeb
 mkdir Gits; cd Gits
 
 # Installation de zimg
 git clone https://github.com/sekrit-twc/zimg.git; cd zimg
-git checkout v2.8
 ./autogen.sh && ./configure && make -j$(nproc)
-su -c "make install"; cd ..
+sudo make install; cd ..
 
 # Installation de Vapoursynth
 git clone https://github.com/vapoursynth/vapoursynth.git; cd vapoursynth
-git checkout R43
 ./autogen.sh && ./configure && make -j$(nproc)
-su -c "make install"; cd ..
+sudo make install; cd ..
 
 # Installation de Vapoursynth Editor
 if [ ! -z $DISPLAY ]; then
 	git clone https://bitbucket.org/mystery_keeper/vapoursynth-editor.git; cd vapoursynth-editor/pro
 	qmake -qt5 && make -j$(nproc); cd ..
-	su -c "cp build/release-64bit-gcc/vsedit /usr/local/bin/vsedit 
-	install -D build/release-64bit-gcc/vsedit.svg /usr/local/share/pixmaps/vsedit.svg
-	if [ ! -d /usr/local/share/applications ]; then mkdir /usr/local/share/applications; fi
-	wget https://gist.githubusercontent.com/YamashitaRen/4489ab810ee92f2fbbf7/raw/d38d73141eccafbeb936c9499fc3f10a885a3a42/vsedit.desktop -P /usr/local/share/applications"
+	sudo cp build/release-64bit-gcc/vsedit /usr/local/bin/vsedit 
+	sudo install -D build/release-64bit-gcc/vsedit.svg /usr/local/share/pixmaps/vsedit.svg
+	if [ ! -d /usr/local/share/applications ]; then sudo mkdir /usr/local/share/applications; fi
+	sudo wget https://gist.githubusercontent.com/YamashitaRen/4489ab810ee92f2fbbf7/raw/2ac6f4da0599d0b5e1166dd7458a689f8a5a2206/vsedit.desktop -P /usr/local/share/applications
 	cp /usr/local/share/applications/vsedit.desktop /home/$USER/$Desktop/vsedit.desktop
 	cd ..
 fi
 
 # Création du lien symbolique FFMS2 dans le dossier plugins de Vapoursynth
-su -c "ln -s $(dpkg-query -L libffms2-4 | grep libffms2.so | tail -1) /usr/local/lib/vapoursynth/libffms2.so"
+sudo ln -s $(dpkg-query -L libffms2-4 | grep libffms2.so | tail -1) /usr/local/lib/vapoursynth/libffms2.so
 
 # Installation de HAvsFunc, mvsfunc et adjust
-git clone https://github.com/HomeOfVapourSynthEvolution/havsfunc.git; cd havsfunc; git checkout r31; cd ..
+git clone https://github.com/HomeOfVapourSynthEvolution/havsfunc.git; cd havsfunc; cd ..
 git clone https://github.com/HomeOfVapourSynthEvolution/mvsfunc.git
 git clone https://github.com/dubhater/vapoursynth-adjust.git
-su -c "cp havsfunc/havsfunc.py mvsfunc/mvsfunc.py vapoursynth-adjust/adjust.py /usr/local/lib/python3.5/site-packages/"
+sudo cp havsfunc/havsfunc.py mvsfunc/mvsfunc.py vapoursynth-adjust/adjust.py /usr/local/lib/python3.8/site-packages/
 
 # Installation de fmtconv
-git clone https://github.com/EleonoreMizo/fmtconv.git; cd fmtconv; git checkout r22; cd build/unix
+git clone https://github.com/EleonoreMizo/fmtconv.git; cd fmtconv; cd build/unix
 ./autogen.sh && ./configure --libdir=/usr/local/lib/vapoursynth && make -j$(nproc)
-su -c "make install"; cd ../../..
+sudo make install; cd ../../..
 
 # Installation de znedi3
 git clone --recursive https://github.com/sekrit-twc/znedi3.git; cd znedi3
 make -j$(nproc) X86=1
-su -c "cp vsznedi3.so /usr/local/lib/vapoursynth/"; cd ..
+sudo cp vsznedi3.so /usr/local/lib/vapoursynth/; cd ..
 
 # Installation de edi_rpow2
 git clone https://gist.github.com/020c497524e794779d9c.git vapoursynth-edi_rpow2
-su -c "cp vapoursynth-edi_rpow2/edi_rpow2.py /usr/local/lib/python3.5/site-packages/edi_rpow2.py"
+sudo cp vapoursynth-edi_rpow2/edi_rpow2.py /usr/local/lib/python3.8/site-packages/edi_rpow2.py
 
 # Vapoursynth doit fonctionner
 if [[ $1 != eng-only ]]
-	then echo -e '\n# Nécessaire pour Vapoursynth\nexport PYTHONPATH=/usr/local/lib/python3.5/site-packages' >> ~/.bashrc
-	else echo -e '\n# Required for Vapoursynth\nexport PYTHONPATH=/usr/local/lib/python3.5/site-packages' >> ~/.bashrc
+	then echo -e '\n# Nécessaire pour Vapoursynth\nexport PYTHONPATH=/usr/local/lib/python3.8/site-packages' >> ~/.bashrc
+	else echo -e '\n# Required for Vapoursynth\nexport PYTHONPATH=/usr/local/lib/python3.8/site-packages' >> ~/.bashrc
 fi
 
 # Éviter un reboot
-su -c "ldconfig"
+sudo ldconfig
 if [[ $1 != eng-only ]]
 	then echo "Script d'installation terminé."
 	else echo "Installation script finished."
