afba301346876d88b5d302b3894cf9fda5be40be
Ryu1845 <ryu@tpgjbo.xyz>
2021-04-05 14:16:28 +0200

    (not so) rough port generate_timecodes
    What does this commit do?
    =========================
    * Roughly port the function generating timecodes

--- yolocr.py
+++ yolocr.py
@@ -3,7 +3,9 @@ import pytesseract
 import logging
 import os
 import shutil
-
+import subprocess
+import re
+import asyncio
 
 logging.basicConfig(level=logging.DEBUG)
 try:
@@ -41,7 +43,7 @@ except FileExistsError:
     shutil.rmtree("tess_result")
     os.mkdir("tess_result")
 
-has_alt = os.path.exists("scene_changes_alt.log")
+HAS_ALT = os.path.exists("scene_changes_alt.log")
 
 if LANG == "fra":
     logging.info("Utilisation de YoloCR en mode CLI.")
@@ -58,3 +60,50 @@ def convert_secs(rough_time: str) -> None:
     s = secs % 60
     ms = int(rough_time.split(".")[1])
     print(f"{h:02}{m:02}{s:02}{ms:03}")
+
+
+def generate_timecodes():
+    args = [
+        "ffprobe",
+        FILTERED_VIDEO,
+        "-v",
+        "0",
+        "-select_streams",
+        "v",
+        "-print_format",
+        "flat",
+        "-show_entries",
+        "stream=r_frame_rate",
+    ]
+    output_ffprobe = subprocess.check_output(args)
+    output_digits = re.findall(r"\d+", str(output_ffprobe))
+    fps = int(output_digits[1]) / int(output_digits[2])
+    logging.info(f"FPS is {fps}")
+    with open("scene_changes.log", "r") as scene_changes_io:
+        timecodes = []
+        frames = scene_changes_io.readlines()
+        for line in frames:
+            if int(line.split(" ")[2]) == 0:
+                timecodes.append(f"{float(line.split(' ')[0])/fps:.4f}"[:-1])
+            elif int(line.split(" ")[1]) == 0:
+                timecodes.append(f"{float(line.split(' ')[0])/fps:.4f}"[:-1])
+    with open("timecodes.txt", "w") as timecodes_io:
+        timecodes.sort(key=float)
+        for frame_time in timecodes:
+            timecodes_io.write(f"{frame_time}\n")
+    if HAS_ALT:
+        with open("scene_changes_alt.log", "r") as scene_changes_io:
+            timecodes = []
+            frames = scene_changes_io.readlines()
+            for line in frames:
+                if int(line.split(" ")[2]) == 0:
+                    timecodes.append(f"{float(line.split(' ')[0])/fps:.4f}"[:-1])
+                elif int(line.split(" ")[1]) == 0:
+                    timecodes.append(f"{float(line.split(' ')[0])/fps:.4f}"[:-1])
+        with open("timecodes_alt.txt", "w") as timecodes_io:
+            timecodes.sort(key=float)
+            for frame_time in timecodes:
+                timecodes_io.write(f"{frame_time}\n")
+
+
+generate_timecodes()
