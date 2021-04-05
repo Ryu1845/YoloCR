03850e75307a06ed110396cbb292d1a0f8ca642a
Ryu1845 <ryu@tpgjbo.xyz>
2021-04-05 11:49:15 +0200

    Rough Port convert_secs
    What does this commit do?
    =========================
    * Port convert_secs
    
    Why are we doing this?
    ======================
    * to eliminate bash
    
    Notes
    =====

--- yolocr.py
+++ yolocr.py
@@ -1,1 +1,7 @@
-import click
+def convert_secs(secs):
+    secs = secs.split(".")[0]
+    h = secs // 3600
+    m = (secs % 3600) // 60
+    s = secs % 60
+    ms = secs.split(".")[1]
+    print(f"{h:02}{m:02}{s:02}{ms:03}")
