2c879ad373f7b4dde3a5f48bb43fe48ff308b9b9
Ryu1845 <ryu@tpgjbo.xyz>
2021-04-05 12:54:06 +0200

    rough port error_handling and init
    What does this commit do?
    =========================
    * Set globals function
    
    Why are we doing this?
    ======================
    * To get rid of bash
    
    Notes
    =====

--- yolocr.py
+++ yolocr.py
@@ -1,7 +1,57 @@
-def convert_secs(secs):
-    secs = secs.split(".")[0]
+import sys
+import pytesseract
+import logging
+import os
+import shutil
+
+
+def convert_secs(rough_time: str) -> None:
+    secs = int(rough_time.split(".")[0])
     h = secs // 3600
     m = (secs % 3600) // 60
     s = secs % 60
-    ms = secs.split(".")[1]
+    ms = int(rough_time.split(".")[1])
     print(f"{h:02}{m:02}{s:02}{ms:03}")
+
+
+logging.basicConfig(level=logging.DEBUG)
+try:
+    LANG = sys.argv[2]
+except IndexError:
+    logging.info("Language not defined. Defaulting to English")
+    LANG = "eng"
+
+try:
+    FILTERED_VIDEO = sys.argv[1]
+except IndexError:
+    if LANG == "fra":
+        raise IndexError(
+            f"""
+N'oubliez pas de mettre le nom de la Vidéo Filtrée en argument.
+Exemple : {sys.argv[0]} Vidéo_Filtrée.mp4 {LANG}
+            """
+        )
+    raise IndexError(
+        f"""
+Don't forget to put the name of the filtered video in the arguments
+Example : {sys.argv[0]} filtered_video.mp4 {LANG}
+        """
+    )
+
+try:
+    os.mkdir("filtered_scsht")
+except FileExistsError:
+    shutil.rmtree("filtered_scsht")
+    os.mkdir("filtered_scsht")
+
+try:
+    os.mkdir("tess_result")
+except FileExistsError:
+    shutil.rmtree("tess_result")
+    os.mkdir("tess_result")
+
+has_alt = os.path.exists("scene_changes_alt.log")
+
+if LANG == "fra":
+    logging.info("Utilisation de YoloCR en mode CLI.")
+    print("Prélude")
