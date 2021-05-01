91b85af747610221032fb47b1009edef5d845991
Ryu1845 <ryu@tpgjbo.xyz>
2021-05-01 19:30:14 +0200

    changed preview layout

--- GUI.py
+++ GUI.py
@@ -5,9 +5,9 @@ import os
 import subprocess
 
 import cv2
+import json
 import PySimpleGUIQt as sg
 import toml
-from PIL import Image, ImageDraw
 
 import yolocr
 
@@ -31,6 +31,14 @@ def gen_scsht(video, frame_time):
     subprocess.run(cmd)
 
 
+def get_vid_info(video):
+    cmd = "ffprobe -v quiet -print_format json -show_format -show_streams".split()
+    cmd.append(video)
+    result = subprocess.run(cmd, capture_output=True, text=True)
+    info = json.loads(result.stdout)
+    return info
+
+
 def convert_to_bytes(path, height, crop_box, height_alt=-1, resize=None):
     img = cv2.imread(path)
     h_img, w_img, _ = img.shape
@@ -47,7 +55,10 @@ def convert_to_bytes(path, height, crop_box, height_alt=-1, resize=None):
         image = cv2.rectangle(image, start_pt2, end_pt2, color, thickness=thickness)
 
     if resize:
-        image = cv2.resize(image, resize)
+        new_w = int(w_img * resize / 100)
+        new_h = int(h_img * resize / 100)
+        new_dim = (new_w, new_h)
+        image = cv2.resize(image, new_dim, interpolation=cv2.INTER_LANCZOS4)
 
     _, im_buf_arr = cv2.imencode(".png", image)
     byte_im = im_buf_arr.tobytes()
@@ -55,11 +66,25 @@ def convert_to_bytes(path, height, crop_box, height_alt=-1, resize=None):
 
 
 def main(config):
+    info = get_vid_info(config["source_file"])
+    duration = float(info["format"]["duration"])
+
     # All the stuff inside your window. This is the PSG magic code compactor...
     source_file_row = [
         sg.Text("Source File Path"),
         sg.InputText(key="source_file", default_text=config["source_file"]),
         sg.FileBrowse(),
+    ]
+    time_scsht_row = [
+        sg.T("Screenshot Time"),
+        sg.Slider(
+            (0, int(duration)),
+            key="time_scsht",
+            enable_events=True,
+            orientation="h",
+            size=(4, 0.3),
+        ),
+        sg.T("0", key="time_scsht_lbl"),
     ]
     cb_dimension_row = [
         sg.Text("Crop Box Dimensions"),
@@ -72,7 +97,7 @@ def main(config):
         ),
         sg.T(
             config["crop"]["crop_box_dimension"][0],
-            key="crop_box_width_label",
+            key="crop_box_width_lbl",
         ),
         sg.Slider(
             (0, 1080),
@@ -83,7 +108,7 @@ def main(config):
         ),
         sg.T(
             config["crop"]["crop_box_dimension"][1],
-            key="crop_box_height_label",
+            key="crop_box_height_lbl",
         ),
     ]
     height_cb_row = [
@@ -98,7 +123,7 @@ def main(config):
         ),
         sg.T(
             config["crop"]["crop_box_height"],
-            key="height_crop_box_label",
+            key="height_crop_box_lbl",
         ),
     ]
     height_alt_cb_row = [
@@ -114,7 +139,7 @@ def main(config):
         ),
         sg.T(
             config["crop"]["crop_box_height_alt"],
-            key="height_crop_box_alt_label",
+            key="height_crop_box_alt_lbl",
         ),
     ]
     crop_frame = [
@@ -124,7 +149,6 @@ def main(config):
                 cb_dimension_row,
                 height_cb_row,
                 height_alt_cb_row,
-                [sg.Button("Preview Settings")],
             ],
         )
     ]
@@ -140,7 +164,7 @@ def main(config):
         ),
         sg.Text(
             config["upscale"]["supersampling_factor"],
-            key="ss_factor_label",
+            key="ss_factor_lbl",
         ),
     ]
     upscale_mode_row = [
@@ -174,7 +198,7 @@ def main(config):
         ),
         sg.Text(
             config["threshold"]["inline_threshold"],
-            key="i_threshold_label",
+            key="i_threshold_lbl",
         ),
     ]
     outline_threshold_row = [
@@ -189,7 +213,7 @@ def main(config):
         ),
         sg.Text(
             config["threshold"]["outline_threshold"],
-            key="o_threshold_label",
+            key="o_threshold_lbl",
         ),
     ]
     scd_threshold_row = [
@@ -204,7 +228,7 @@ def main(config):
         ),
         sg.Text(
             config["threshold"]["SCD_threshold"],
-            key="SCD_threshold_label",
+            key="SCD_threshold_lbl",
         ),
     ]
     threshold_frame = [
@@ -220,21 +244,45 @@ def main(config):
     ]
     settings_col = [
         source_file_row,
+        time_scsht_row,
+        [sg.Button("Generate Screenshot")],
         crop_frame,
         upscale_frame,
         threshold_frame,
         [sg.Checkbox("Preview", key="has_preview", enable_events=True)],
-        [sg.Submit(), sg.Cancel()],
+        [sg.Button("Save Settings"), sg.Cancel()],
     ]
     preview_col = [
         [sg.Image(key="preview_image")],
     ]
+    # layout = [
+    #     [
+    #         sg.Column(settings_col, element_justification="c"),
+    #         sg.VSeperator(),
+    #         sg.Col(preview_col, element_justification="c"),
+    #     ]
+    # ]
     layout = [
+        source_file_row,
+        time_scsht_row,
+        [sg.Button("Generate Screenshot")],
+        crop_frame,
+        upscale_frame,
+        threshold_frame,
         [
-            sg.Column(settings_col, element_justification="c"),
-            sg.VSeperator(),
-            sg.Col(preview_col, element_justification="c"),
-        ]
+            sg.Checkbox("Preview", key="has_preview", enable_events=True),
+            sg.Slider(
+                (0, 200),
+                key="resize_factor",
+                enable_events=True,
+                size=(4, 0.3),
+                orientation="h",
+                default_value=30,
+            ),
+            sg.T("30%", key="resize_factor_lbl"),
+        ],
+        [sg.Button("Save Settings"), sg.Cancel()],
+        [sg.Image(key="preview_image")],
     ]
 
     # Create the Window
@@ -246,20 +294,23 @@ def main(config):
         if event in (sg.WIN_CLOSED, "Cancel"):
             break
         if values["has_preview"]:
-            if not os.path.exists("screenshot.png"):
-                gen_scsht(values["source_file"], 12)
-            if (
+            if not os.path.exists("screenshot.png") or event == "Generate Screenshot":
+                gen_scsht(values["source_file"], values["time_scsht"])
+
+            new_vals = (
                 values["height_crop_box"],
                 values["crop_box_width"],
                 values["crop_box_height"],
                 values["height_crop_box_alt"],
-            ) != prev_vals:
+            )
+            if new_vals != prev_vals or event == "Generate Screenshot":
                 window["preview_image"].update(
                     data=convert_to_bytes(
                         "screenshot.png",
                         values["height_crop_box"],
                         (values["crop_box_width"], values["crop_box_height"]),
                         height_alt=values["height_crop_box_alt"],
+                        resize=values["resize_factor"],
                     )
                 )
                 prev_vals = (
@@ -271,14 +322,19 @@ def main(config):
         else:
             window["preview_image"].update(data=bytes())
 
-        window["crop_box_width_label"].update(values["crop_box_width"])
-        window["crop_box_height_label"].update(values["crop_box_height"])
-        window["height_crop_box_label"].update(values["height_crop_box"])
-        window["height_crop_box_alt_label"].update(values["height_crop_box_alt"])
-        window["ss_factor_label"].update(values["supersampling_factor"])
-        window["i_threshold_label"].update(values["inline_threshold"])
-        window["o_threshold_label"].update(values["outline_threshold"])
-        window["SCD_threshold_label"].update(values["SCD_threshold"] / 100)
+        m, s = divmod(values["time_scsht"], 60)
+        h, m = divmod(m, 60)
+        readable_time_scsht = f"{h:d}:{m:02d}:{s:02d}"
+        window["time_scsht_lbl"].update(readable_time_scsht)
+        window["crop_box_width_lbl"].update(values["crop_box_width"])
+        window["crop_box_height_lbl"].update(values["crop_box_height"])
+        window["height_crop_box_lbl"].update(values["height_crop_box"])
+        window["height_crop_box_alt_lbl"].update(values["height_crop_box_alt"])
+        window["ss_factor_lbl"].update(values["supersampling_factor"])
+        window["i_threshold_lbl"].update(values["inline_threshold"])
+        window["o_threshold_lbl"].update(values["outline_threshold"])
+        window["SCD_threshold_lbl"].update(values["SCD_threshold"] / 100)
+        window["resize_factor_lbl"].update(f"{values['resize_factor']}%")
 
         config = {
             "source_file": values["source_file"],
@@ -305,7 +361,7 @@ def main(config):
         }
         with open("config.tmp.toml", "w") as config_io:
             toml.dump(config, config_io)
-        if event == "Submit":
+        if event == "Save Settings":
             os.rename("config.tmp.toml", "config.toml")
 
     window.close()
