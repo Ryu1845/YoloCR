5f63f69ee7f9e753aad767fdb1026dc0dc3967a4
Ryu18 <ryu@tpgjbo.xyz>
2021-07-06 19:35:20 +0200

    rewrite yoloresize

--- yolocr/YoloResize.py
+++ yolocr/YoloResize.py
@@ -1,103 +1,27 @@
-# TODO fix the position of the crop box
-
-import toml
 import vapoursynth as vs
+import toml
+import cv2
+import numpy as np
 
+config = toml.load('../config.toml')
 core = vs.get_core()
-config_path = "../config.toml"
-config = toml.load(config_path)
-SOURCE_FILE = config["source_file"]
-CROP_BOX_DIMENSIONS = config["crop"]["crop_box_dimension"]
-HEIGHT_CROP_BOX = config["crop"]["crop_box_height"]
-SS_FACTOR = config["upscale"]["supersampling_factor"]
-UPSCALE_MODE = config["upscale"]["upscale_mode"]
-if UPSCALE_MODE == "znedi3":
-    import edi_rpow2 as edi
+clip = core.ffms2.Source(config['source_file'])
+crop_box_dimension = config['crop']['crop_box_dimension']
+height_crop_box = config['crop']['crop_box_height']
 
 
-def supersampling(clip, factor, factor_bis):
-    if factor != 1:
-        if UPSCALE_MODE in ("znedi3", "waifu2x"):
-            if UPSCALE_MODE == "znedi3":
-                clip = edi.znedi3_rpow2(clip=clip, rfactor=factor)
-            else:
-                clip = core.fmtc.bitdepth(clip=clip, bits=32)
-                clip = core.w2xc.Waifu2x(clip=clip, scale=factor)
-            if factor_bis != 1:
-                clip = core.fmtc.resample(
-                    clip=clip, scale=factor_bis, kernel="sinc", taps=2
-                )
-        else:
-            clip = core.fmtc.resample(clip=clip, scale=factor, kernel="sinc", taps=2)
-    clip = core.fmtc.resample(clip=clip, css="444")
-    clip = core.fmtc.bitdepth(clip=clip, bits=8)
-    return clip
+def draw_box(n, f):
+    frame = f
+    frame = [frame]
+    fout = frame[0].copy()
+    arr = np.asarray(frame[0].get_read_array(0))
+    pt1 = ((f.width-crop_box_dimension[0])//2, f.height-(height_crop_box+crop_box_dimension[1]))
+    pt2 = ((f.width+crop_box_dimension[0])//2, f.height-height_crop_box)
+    output = cv2.rectangle(arr, pt1, pt2, (125, 125, 125), thickness=20)
+    out_arr = np.asarray(fout.get_write_array(0))
+    np.copyto(out_arr, output)
+    return fout
 
 
-def main():
-    source_clip = core.ffms2.Source(source=SOURCE_FILE)
-    crop_box = core.std.CropAbs(
-        clip=source_clip,
-        width=CROP_BOX_DIMENSIONS[0],
-        height=CROP_BOX_DIMENSIONS[1],
-        left=int((source_clip.width - CROP_BOX_DIMENSIONS[0]) / 2),
-        top=source_clip.height - HEIGHT_CROP_BOX - CROP_BOX_DIMENSIONS[1],
-    )
-    clip_gray = core.std.Lut(clip=source_clip, planes=[1, 2], function=lambda x: 128)
-
-    if SS_FACTOR < 0:
-        if source_clip.width / source_clip.height > 16 / 9:
-            target_res = 1920
-            current_res = source_clip.width
-        else:
-            target_res = 1080
-            current_res = source_clip.height
-        if UPSCALE_MODE == "znedi3":
-            ss = target_res / current_res / 1.125
-        else:
-            ss = target_res / current_res
-    elif SS_FACTOR == 0:
-        ss = 1
-    else:
-        ss = SS_FACTOR
-
-    if UPSCALE_MODE == "znedi3" and ss != 1:
-        if ss - int(ss) > 0:
-            ss = int(ss / 2) * 2 + 2
-        else:
-            ss = int(ss / 2) * 2
-        if SS_FACTOR < 0:
-            ssbis = target_res / (current_res * ss)
-        else:
-            ssbis = SS_FACTOR / ss
-
-    crop_box = supersampling(crop_box, ss, ssbis)
-    clip_gray = supersampling(clip_gray, ss, ssbis)
-
-    clip_left = core.std.Crop(
-        clip=clip_gray, right=int((crop_box.width + clip_gray.width) / 2)
-    )
-    clip_right = core.std.Crop(clip=clip_gray, left=clip_left.width + crop_box.width)
-    clip = core.std.CropAbs(
-        clip=clip_gray,
-        width=crop_box.width,
-        height=clip_gray.height,
-        left=int((clip_gray.width - crop_box.width) / 2),
-    )
-    clip_top = core.std.Crop(
-        clip=clip, bottom=int(HEIGHT_CROP_BOX * ss) + crop_box.height
-    )
-    clip_bottom = core.std.Crop(clip=clip, top=clip_top.height + crop_box.height)
-
-    edge = int(5 * ss)
-    crop = core.std.Crop(clip=crop_box, left=edge, right=edge, top=edge, bottom=edge)
-    rect = core.std.AddBorders(
-        clip=crop, left=edge, right=edge, top=edge, bottom=edge, color=[255, 128, 128]
-    )
-    clip = core.std.StackVertical([clip_top, rect, clip_bottom])
-    clip = core.std.StackHorizontal([clip_left, clip, clip_right])
-    clip.set_output()
-
-
-if __name__ == "__vapoursynth__":
-    main()
+clip = core.std.ModifyFrame(clip, clip, draw_box)
+clip.set_output()
